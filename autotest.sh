3f6d96ceeeb659616b2897fc1e218e0cac084e18
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-10 10:09:46 -0700

    Update autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -6,6 +6,15 @@ TESTED=0
 FAILED=0
 FILES=
 
+for OPENFIDO_INPUT in $(find $PWD/autotest -name 'input_*' -print); do
+        echo "Processing $OPENFIDO_INPUT..."
+        export OPENFIDO_INPUT
+        export OPENFIDO_OUTPUT=${OPENFIDO_INPUT/autotest\/input_/autotest\/output_}
+        mkdir -p $OPENFIDO_OUTPUT
+        rm -rf $OPENFIDO_OUTPUT/{*,.??*}
+        TESTED=$(($TESTED+1))
+    done
+    
 echo "$TESTED tests completed"
 echo "$FAILED tests failed"
 if [ $FAILED -gt 0 ]; then
