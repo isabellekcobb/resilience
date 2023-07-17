6bb313220a9d0c3eb2175bbbbae2d5031aa558be
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-07-17 15:16:12 -0700

    Update openfido.sh

--- openfido.sh
+++ openfido.sh
@@ -77,7 +77,7 @@ else
     USECASE="all"
     POLE_NAME=""
 fi
- gridlabd geodata merge -D census 37.5,-122.2 37.4,-122.3 -o $OPENFIDO_OUTPUT/census.csv 
+ 
 
 if [ "$ANALYSIS" = "vegetation_analysis" ]; then 
     echo "Running vegetation analysis, only."
@@ -91,6 +91,7 @@ if [ "$ANALYSIS" = "vegetation_analysis" ]; then
     cut -d ',' -f 2 "$OPENFIDO_INPUT/$POLE_DATA" >$OPENFIDO_OUTPUT/longitude.csv
     LATITUDE=$(grep ^LATITUDE, "latitude.csv" | cut -f1- -d, | tr ',' ' ')
     LONGITUDE=$(grep ^LONGITUDE, "longitude.csv" | cut -f1- -d, | tr ',' ' ')
+    gridlabd geodata merge -D census LATITUDE,LONGITUDE -o $OPENFIDO_OUTPUT/test_zipcode.csv 
     
     python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/income_data.py
 elif [ "$ANALYSIS" = "pole_analysis" ]; then 
