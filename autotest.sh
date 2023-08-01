6a63ecc320162d5f10b247cb27fe3d23ce7708b7
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-01 11:07:17 -0700

    Update autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -12,7 +12,7 @@ for OPENFIDO_INPUT in $(find $PWD/autotest -name 'input_*' -print); do
         export OPENFIDO_OUTPUT=${OPENFIDO_INPUT/autotest\/input_/autotest\/output_}
         mkdir -p $OPENFIDO_OUTPUT
         rm -rf $OPENFIDO_OUTPUT/{*,.??*}
-        if ! sh < openfido.sh 1>$OPENFIDO_OUTPUT/stdout 2>$OPENFIDO_OUTPUT/stderr; then
+        if ! bash < openfido.sh 1>$OPENFIDO_OUTPUT/stdout 2>$OPENFIDO_OUTPUT/stderr; then
         FAILED=$(($FAILED+1)) 
         FILES="$FILES ${OPENFIDO_OUTPUT/$PWD\//}"
         echo "ERROR: $OPENFIDO_INPUT test failed"
@@ -23,8 +23,8 @@ for OPENFIDO_INPUT in $(find $PWD/autotest -name 'input_*' -print); do
 echo "$TESTED tests completed"
 echo "$FAILED tests failed"
 if [ $FAILED -gt 0 ]; then
-    tar cfz autotest-errors.tar.gz $FILES
-    echo "Failure artifacts saved to autotest-errors.tar.gz"
+    tar cf autotest-errors.tar $FILES
+    echo "Failure artifacts saved to autotest-errors.tar"
     exit 1
 else
     echo "Success"
