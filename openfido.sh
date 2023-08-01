2c65c8b88b8ef1ec14700f44bd98568bc904b9f1
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-01 14:18:32 -0700

    Update openfido.sh

--- openfido.sh
+++ openfido.sh
@@ -125,7 +125,9 @@ elif [ "$ANALYSIS" = "pole_analysis" ]; then
     # gridlabd /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/model.glm
     # python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/critical_load2.py
     # python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/weather.py
-    python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/group.py -i=network.json --modify -o=groups.glm --force
+    # python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/group.py -i=network.json --modify -o=groups.glm --force
+    gridlabd -C 123.glm -o 123.json
+    python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/group.py -i=123.json --modify -o=groups.glm --force
 fi 
 
 
