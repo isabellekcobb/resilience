74c0c47d6d7af6449079d15705eaa9d333a6c5a2
isabellekcobb <137833895+isabellekcobb@users.noreply.github.com>
2023-08-04 10:20:29 -0700

    Update openfido.sh

--- openfido.sh
+++ openfido.sh
@@ -57,6 +57,7 @@ cp -R $OPENFIDO_INPUT/* . #WHY?
 if [ -e "config.csv" ]; then
     ANALYSIS=$(grep ^ANALYSIS, "config.csv" | cut -f2- -d, | tr ',' ' ')
     POLE_DATA=$(grep ^POLE_DATA, "config.csv" | cut -f2- -d, | tr ',' ' ')
+    VEGETATION_DATA=$(grep ^VEGETATION_DATA, "config.csv" | cut -f2- -d, | tr ',' ' ')
     USECASE=$(grep ^USECASE, "config.csv" | cut -f2- -d, | tr ',' ' ')
     POLE_NAME=$(grep ^POLE_NAME, "config.csv" | cut -f2- -d, | tr ',' ' ')
     WIND_SPEED=$(grep ^WIND_SPEED, "config.csv" | cut -f2- -d, | tr ',' ' ')
@@ -81,7 +82,7 @@ fi
 
 if [ "$ANALYSIS" = "vegetation_analysis" ]; then 
     echo "Running vegetation analysis, only."
-    gridlabd geodata merge -D elevation $OPENFIDO_INPUT/$POLE_DATA -r 30 | gridlabd geodata merge -D vegetation >$OPENFIDO_OUTPUT/path_vege.csv
+    gridlabd geodata merge -D elevation $OPENFIDO_INPUT/$VEGETATION_DATA -r 30 | gridlabd geodata merge -D vegetation >$OPENFIDO_OUTPUT/path_vege.csv
     python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/add_info.py # this needs to get integrated into the gridlabd source code
     gridlabd geodata merge -D powerline $OPENFIDO_OUTPUT/path_vege.csv --cable_type="TACSR/AC 610mm^2" >$OPENFIDO_OUTPUT/path_result.csv
 
@@ -127,7 +128,48 @@ elif [ "$ANALYSIS" = "pole_analysis" ]; then
     # python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/group.py -i=123.json --modify -o=groups.glm --force
     python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/critical_load.py network.json
 fi 
+// added use case
+elif [ "$ANALYSIS" = "pole+veg_analysis" ]; then 
+    gridlabd geodata merge -D elevation $OPENFIDO_INPUT/$VEGETATION_DATA -r 30 | gridlabd geodata merge -D vegetation >$OPENFIDO_OUTPUT/path_vege.csv
+    python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/add_info.py # this needs to get integrated into the gridlabd source code
+    gridlabd geodata merge -D powerline $OPENFIDO_OUTPUT/path_vege.csv --cable_type="TACSR/AC 610mm^2" >$OPENFIDO_OUTPUT/path_result.csv
 
+    cut -d ',' -f 3 $OPENFIDO_OUTPUT/path_result.csv >$OPENFIDO_OUTPUT/latitude.csv
+    cut -d ',' -f 4 $OPENFIDO_OUTPUT/path_result.csv>$OPENFIDO_OUTPUT/longitude.csv
+    python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/testing_geopandas.py
+
+    python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/folium_data.py
+    gridlabd /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/folium.glm -D html_save_options="--cluster" -o $OPENFIDO_OUTPUT/folium.html
+    python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/prioritize.py
+    
+    CSV_NAME="poles_w_equip_and_network"
+    GLM_NAME="network"
+    USECASES=("loading_scenario" "critical_speed" "worst_angle")
+    RESULT_NAME="results"
+    POLE_OPTION=""
+    if [[ -n "$POLE_NAME" ]]; then
+        POLE_OPTION="--poles_selected=pole_$POLE_NAME"
+        POLE_NAME="$POLE_NAME\_"
+    fi
+
+    echo "Converting SPIDAcalc excel report to CSV"
+    gridlabd convert $OPENFIDO_INPUT/$POLE_DATA $OPENFIDO_OUTPUT/$CSV_NAME.csv -f xls-spida -t csv-geodata extract_equipment=yes include_network=yes
+    echo "Converting CSV to GLM"
+    gridlabd -D csv_load_options="-f table -t object -M powerflow -o $OPENFIDO_OUTPUT/$GLM_NAME.glm" $OPENFIDO_OUTPUT/$CSV_NAME.csv
+    echo "Pole analysis on GLM file"
+    if [[ "$USECASE" = "all" ]]; then
+        for option in "${USECASES[@]}"; do
+        echo "Running $option usecase"
+            gridlabd pole_analysis $OPENFIDO_OUTPUT/$GLM_NAME.glm --analysis=$option --wind_speed=$WIND_SPEED --wind_direction=$WIND_DIR --direction_increment=$WIND_DIR_INC --speed_increment=$WIND_SPEED_INC --segment=$POLE_DIV --output=$OPENFIDO_OUTPUT/$RESULT_NAME\_$POLE_NAME$option.csv $POLE_OPTION
+        done
+    else
+        gridlabd pole_analysis $OPENFIDO_OUTPUT/$GLM_NAME.glm --analysis=$USECASE --wind_speed=$WIND_SPEED --wind_direction=$WIND_DIR --direction_increment=$WIND_DIR_INC --speed_increment=$WIND_SPEED_INC --segment=$POLE_DIV --output=$OPENFIDO_OUTPUT/$RESULT_NAME\_$POLE_NAME$USECASE.csv $POLE_OPTION
+    fi
+    python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/group.py -i=network.json --modify -o=groups.glm --force
+    # gridlabd -C 123.glm -o 123.json
+    # python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/group.py -i=123.json --modify -o=groups.glm --force
+    python3 /usr/local/share/gridlabd/template/US/CA/SLAC/anticipation/critical_load.py network.json
+fi 
 
 # ( gridlabd template $TEMPLATE_CFG get $TEMPLATE && gridlabd --redirect all $OPTIONS -t $TEMPLATE  ) || error
 
